2b5222cdb5594446e55cea43eb47c0b399cff219
viol8888 <viol8888@users.noreply.github.com>
2026-03-02 17:18:15 +0000

    Fix: implement missing GetTestInputData for image_smoothing

--- tasks/bruskova_v_image_smoothing/tests/functional/main.cpp
+++ tasks/bruskova_v_image_smoothing/tests/functional/main.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-
 #include <tuple>
 #include <vector>
 
@@ -14,14 +13,17 @@ class BruskovaVImageSmoothingFuncTests : public ppc::util::BaseRunFuncTests<InTy
  protected:
   void SetUp() override {
     auto test_params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-
     int size = std::get<0>(test_params);
     input_data_ = std::vector<int>(size, 128);
-    expected_output_ = std::vector<int>(size, 128);
+    expected_output_ = std::vector<int>(size, 128); 
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
     return output_data.size() == input_data_.size();
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
   }
 
  private:
@@ -34,11 +36,9 @@ TEST_P(BruskovaVImageSmoothingFuncTests, SmoothingTest) {
 
 const std::vector<TestType> kTestParam = {std::make_tuple(10), std::make_tuple(50)};
 
-const auto kTestTasksList =
-    std::tuple_cat(ppc::util::AddFuncTask<BruskovaVImageSmoothingMPI, InType, OutType, TestType>(
-                       kTestParam, "bruskova_v_image_smoothing_mpi"),
-                   ppc::util::AddFuncTask<BruskovaVImageSmoothingSEQ, InType, OutType, TestType>(
-                       kTestParam, "bruskova_v_image_smoothing_seq"));
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<BruskovaVImageSmoothingMPI, InType, OutType, TestType>(kTestParam, "bruskova_v_image_smoothing_mpi"),
+    ppc::util::AddFuncTask<BruskovaVImageSmoothingSEQ, InType, OutType, TestType>(kTestParam, "bruskova_v_image_smoothing_seq"));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
