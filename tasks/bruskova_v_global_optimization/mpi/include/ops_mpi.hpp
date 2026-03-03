f6ac57d51c5a6f880e5baa8cddb1d95e5f2ddd6a
viol8888 <viol8888@users.noreply.github.com>
2026-03-03 08:34:25 +0000

    Fix: correctly replace BaseTask with Task and update types

--- tasks/bruskova_v_global_optimization/mpi/include/ops_mpi.hpp
+++ tasks/bruskova_v_global_optimization/mpi/include/ops_mpi.hpp
@@ -5,10 +5,10 @@
 #include "task/include/task.hpp"
 
 namespace bruskova_v_global_optimization {
-class BruskovaVGlobalOptimizationMPI : public ppc::task::BaseTask<InType, OutType> {
+
+class BruskovaVGlobalOptimizationMPI : public ppc::task::Task<InType, OutType> {
  public:
-  explicit BruskovaVGlobalOptimizationMPI(const ppc::task::TaskData &data)
-      : ppc::task::BaseTask<InType, OutType>(data) {}
+  explicit BruskovaVGlobalOptimizationMPI(const InType& in) : ppc::task::Task<InType, OutType>(in) {}
 
   bool PreProcessingImpl() override;
   bool ValidationImpl() override;
@@ -16,11 +16,12 @@ class BruskovaVGlobalOptimizationMPI : public ppc::task::BaseTask<InType, OutTyp
   bool PostProcessingImpl() override;
 
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-    return ppc::task::TypeOfTask::TASK;
+    return ppc::task::TypeOfTask::kMPI;
   }
 
  private:
   double x_min_ = 0.0, x_max_ = 0.0, y_min_ = 0.0, y_max_ = 0.0, step_ = 0.0;
   OutType result_;
 };
+
 }  // namespace bruskova_v_global_optimization
--- tasks/bruskova_v_global_optimization/seq/include/ops_seq.hpp
+++ tasks/bruskova_v_global_optimization/seq/include/ops_seq.hpp
@@ -2,12 +2,14 @@
 #include <vector>
 
 #include "bruskova_v_global_optimization/common/include/common.hpp"
+#include "task/include/task.hpp"
 
 namespace bruskova_v_global_optimization {
 
-class BruskovaVGlobalOptimizationSEQ : public BaseTask {
+class BruskovaVGlobalOptimizationSEQ : public ppc::task::Task<InType, OutType> {
  public:
-  explicit BruskovaVGlobalOptimizationSEQ(const InType &in);
+  explicit BruskovaVGlobalOptimizationSEQ(const InType& in) : ppc::task::Task<InType, OutType>(in) {}
+
   bool PreProcessingImpl() override;
   bool ValidationImpl() override;
   bool RunImpl() override;
@@ -18,7 +20,7 @@ class BruskovaVGlobalOptimizationSEQ : public BaseTask {
   }
 
  private:
-  double x_min_, x_max_, y_min_, y_max_, step_;
+  double x_min_ = 0.0, x_max_ = 0.0, y_min_ = 0.0, y_max_ = 0.0, step_ = 0.0;
   std::vector<double> result_;
 };
 
